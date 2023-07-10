83813307022d3997e786f38012273423f84187d4
Fazil Babu <fazil47babu@gmail.com>
2023-07-10 17:02:36 +0530

    Started adding side panels to editor

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import BabylonScene from "@/components/BabylonScene";
+import BabylonEditor from "@/components/BabylonEditor";
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen m-0 p-2">
-      <BabylonScene />
+    <main>
+      <BabylonEditor />
     </main>
   );
 }
--- components/BabylonEditor.tsx
+++ components/BabylonEditor.tsx
@@ -5,8 +5,10 @@ import { useEffect, useRef, useState } from "react";
 import BabylonApp from "@/babylon/app";
 import { Engine, Scene, WebGPUEngine } from "@babylonjs/core";
 
-export default function BabylonScene() {
+export default function BabylonEditor() {
   const renderCanvas = useRef<HTMLCanvasElement>(null);
+  const hierarchy = useRef<HTMLDivElement>(null);
+  const inspector = useRef<HTMLDivElement>(null);
 
   const [engine, setEngine] = useState<Engine | WebGPUEngine | null>(null);
   const [scene, setScene] = useState<Scene | null>(null);
@@ -27,13 +29,25 @@ export default function BabylonScene() {
             parentContainer.clientHeight -
             parseFloat(window.getComputedStyle(parentContainer).paddingTop) -
             parseFloat(window.getComputedStyle(parentContainer).paddingBottom);
-          const width =
+          renderCanvas.current.height = height;
+
+          const parentWidth =
             parentContainer.clientWidth -
             parseFloat(window.getComputedStyle(parentContainer).paddingLeft) -
             parseFloat(window.getComputedStyle(parentContainer).paddingRight);
 
-          renderCanvas.current.width = width;
-          renderCanvas.current.height = height;
+          let siblingWidth = 0;
+          if (hierarchy.current) {
+            hierarchy.current.style.height = `${height}px`;
+            siblingWidth += hierarchy.current.clientWidth;
+          }
+
+          if (inspector.current) {
+            inspector.current.style.height = `${height}px`;
+            siblingWidth += inspector.current.clientWidth;
+          }
+
+          renderCanvas.current.width = parentWidth - siblingWidth;
         }
 
         if (engine) {
@@ -76,13 +90,26 @@ export default function BabylonScene() {
           <p className="text-3xl font-bold">Loading...</p>
         </div>
       )}
-      <canvas
-        ref={renderCanvas}
-        className="rounded-md focus:outline-none"
+      <div
+        className="h-screen w-screen m-0 p-2 flex-row justify-center items-center gap-1"
         style={{
-          display: loaded ? "block" : "none",
+          display: loaded ? "flex" : "none",
         }}
-      />
+      >
+        {/* <div ref={hierarchy} className="p-2 flex flex-col items-center rounded-md bg-gray-200 dark:bg-gray-800">
+          <p className="text-3xl font-bold">Scene Hierarchy</p>
+        </div> */}
+        <canvas
+          ref={renderCanvas}
+          className="h-full rounded-md focus:outline-none flex-grow"
+        />
+        <div
+          ref={inspector}
+          className="p-2 flex flex-col items-center rounded-md bg-gray-200 dark:bg-gray-800"
+        >
+          <p className="text-3xl font-bold">Inspector</p>
+        </div>
+      </div>
     </>
   );
 }
